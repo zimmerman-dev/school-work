aaf040deea8eac4376a3d278a8fe89060b363aa4
John Zimmerman <zimmerman-dev@outlook.com>
2026-02-10 01:34:08 -0600

    project part 2 complete - ITPJS

--- Intro Prog JS/LearningUnits/Unit1/unit01/js/project-part02.js
+++ Intro Prog JS/LearningUnits/Unit1/unit01/js/project-part02.js
@@ -3,7 +3,32 @@ function part02() {
 
     // YOUR CODE STARTS AFTER THIS LINE:
 
-    // Delete this line before starting (for testing purposes)
-    document.write("part02");
-    
+    // Variable Declaration
+
+    let adjective;
+    let location;
+    let nickName;
+    let color;
+
+    // Prompt user, return values store to input variables
+
+    adjective = prompt("Enter an adjective:");
+    location = prompt("Enter a location:");
+    nickName = prompt("Enter a silly nick-name:");
+    color = prompt("Enter a color:");
+
+    // Change strings to uppercase
+
+    adjective = adjective.toUpperCase();
+    location = location.toUpperCase();
+    nickName = nickName.toUpperCase();
+    color = color.toUpperCase();
+
+    // Display madlib with values from output variables
+
+    document.writeln("Last night I walked into a " 
+        + adjective + " building near the " + location 
+        + ".\nIt was a dive bar called, \"" + nickName 
+        + "'s,\" and it had a " + color 
+        + "\ndoor with bullet holes in it. It was a crazy night.");
 }
