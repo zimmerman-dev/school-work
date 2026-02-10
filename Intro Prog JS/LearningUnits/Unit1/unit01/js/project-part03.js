5cfee99bc149cda70fd8c1e2ddd3b7e4ebe5480e
John Zimmerman <zimmerman-dev@outlook.com>
2026-02-10 00:42:02 -0600

    project part 3 complete - ITPJS

--- Intro Prog JS/LearningUnits/Unit1/unit01/js/project-part03.js
+++ Intro Prog JS/LearningUnits/Unit1/unit01/js/project-part03.js
@@ -3,7 +3,33 @@ function part03() {
 
     // YOUR CODE STARTS AFTER THIS LINE:
 
-    // Delete this line before starting (for testing purposes)
-    document.write("part03");
-    
+    // Variable Declaration
+
+    let containerOne;
+    let containerTwo;
+    let containerThree;
+
+    // Prompt user for flower names, return values get stored into containers
+
+    containerOne = prompt("Enter a flower name: ");
+    containerTwo = prompt("Enter another flower name: ");
+
+    // Output the entered values from prompt()
+
+    document.writeln("Strings as entered:");
+    document.writeln("containerOne: " + containerOne);
+    document.writeln("containertwo: " + containerTwo);
+    document.writeln(""); // empty line
+
+    // Value-switch
+
+    containerThree = containerOne;
+    containerOne = containerTwo;
+    containerTwo = containerThree;
+
+    // Output the values switched
+
+    document.writeln("Switched strings:");
+    document.writeln("containerOne: " + containerOne);
+    document.writeln("containerTwo: " + containerTwo);
 }
