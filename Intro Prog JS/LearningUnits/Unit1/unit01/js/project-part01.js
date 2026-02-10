7d7ab3685e5fc72bd8c3c97e18f2b324d744da29
John Zimmerman <zimmerman-dev@outlook.com>
2026-02-09 23:54:50 -0600

    project part 1 complete - ITPJS

--- Intro Prog JS/LearningUnits/Unit1/unit01/js/project-part01.js
+++ Intro Prog JS/LearningUnits/Unit1/unit01/js/project-part01.js
@@ -3,7 +3,37 @@ function part01() {
 
     // YOUR CODE STARTS AFTER THIS LINE:
 
-    // Delete this line before starting (for testing purposes)
-    document.write("part01");
-    
+    // Variable Declaration
+
+    let firstNumber;
+    let secondNumber;
+    let sum;
+    let difference;
+    let product;
+    let quotient;
+
+    // Prompt user, return values get stored in input variables
+
+    firstNumber = prompt("Enter the first number: ");
+    secondNumber = prompt("Enter the second number: ");
+
+    // Explicit conversion with Number()
+
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    // Calculations
+
+    sum = firstNumber + secondNumber;
+    difference = firstNumber - secondNumber;
+    product = firstNumber * secondNumber;
+    quotient = firstNumber / secondNumber;
+
+    // Output the values from output variables
+
+    document.writeln("Entered Numbers: " + firstNumber + ", " + secondNumber);
+    document.writeln("Sum: " + sum);
+    document.writeln("Difference: " + difference);
+    document.writeln("Product: " + product);
+    document.writeln("Quotient: " + quotient);
 }
