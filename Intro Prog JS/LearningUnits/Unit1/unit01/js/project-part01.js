d41ed3d1e6444a54db58501f43017e75b1d1de82
John Zimmerman <zimmerman-dev@outlook.com>
2026-02-13 12:35:13 -0600

    updating changes to project

--- Intro Prog JS/LearningUnits/Unit1/unit01/js/project-part01.js
+++ Intro Prog JS/LearningUnits/Unit1/unit01/js/project-part01.js
@@ -1,10 +1,7 @@
 function part01() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
-
     // Variable Declaration
-
     let firstNumber;
     let secondNumber;
     let sum;
@@ -12,25 +9,20 @@ function part01() {
     let product;
     let quotient;
 
-    // Prompt user, return values get stored in input variables
-
+    // Prompt user for two numbers
     firstNumber = prompt("Enter the first number: ");
     secondNumber = prompt("Enter the second number: ");
-
-    // Explicit conversion with Number()
 
     firstNumber = Number(firstNumber);
     secondNumber = Number(secondNumber);
 
-    // Calculations
-
+    // Calculate sum, difference, product, and quotient
     sum = firstNumber + secondNumber;
     difference = firstNumber - secondNumber;
     product = firstNumber * secondNumber;
     quotient = firstNumber / secondNumber;
 
-    // Output the values from output variables
-
+    // Display output variables
     document.writeln("Entered Numbers: " + firstNumber + ", " + secondNumber);
     document.writeln("Sum: " + sum);
     document.writeln("Difference: " + difference);
--- Intro Prog JS/LearningUnits/Unit1/unit01/js/project-part02.js
+++ Intro Prog JS/LearningUnits/Unit1/unit01/js/project-part02.js
@@ -1,31 +1,24 @@
 function part02() {
     "use strict";
-
-    // YOUR CODE STARTS AFTER THIS LINE:
-
+    
     // Variable Declaration
-
     let adjective;
     let location;
     let nickName;
     let color;
 
-    // Prompt user, return values store to input variables
-
+    // Prompt user for adjective, location, nickName, and color
     adjective = prompt("Enter an adjective:");
     location = prompt("Enter a location:");
     nickName = prompt("Enter a silly nick-name:");
-    color = prompt("Enter a color:");
-
-    // Change strings to uppercase
+    color = prompt("Enter a color:");    
 
     adjective = adjective.toUpperCase();
     location = location.toUpperCase();
     nickName = nickName.toUpperCase();
     color = color.toUpperCase();
 
-    // Display madlib with values from output variables
-
+    // Display the madlib with the output variables
     document.writeln("Last night I walked into a " 
         + adjective + " building near the " + location 
         + ".\nIt was a dive bar called, \"" + nickName 
