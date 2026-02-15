02e1e7d30b9ec9ece3778d5e5d1a8d495034385d
John Zimmerman <zimmerman-dev@outlook.com>
2026-02-15 13:40:28 -0600

    Completed challenge 4 ITPJS

--- Intro Prog JS/LearningUnits/Unit2/unit02/unit02/js/exercise-04.js
+++ Intro Prog JS/LearningUnits/Unit2/unit02/unit02/js/exercise-04.js
@@ -1,15 +1,42 @@
 function exercise04Part1() {
     "use strict";
-    // PART 1: YOUR CODE STARTS AFTER THIS LINE:
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Exercise for lab 4, Part 1");
+    // Variable Declarations
+    let firstName;
+    let middleInitial;
+    let lastName;
+    let fullName;
+
+    // Variable Assigments
+    firstName = prompt("Enter first name:");
+    middleInitial = prompt("Enter middle initial:");
+    lastName = prompt("Enter last name:");
+
+    // Processing Section
+    fullName = firstName + " " + middleInitial + ". " + lastName;
+
+    // Display fullName
+    document.writeln(fullName);
 }
 
 function exercise04Part2() {
     "use strict";
-    // PART 2: YOUR CODE STARTS AFTER THIS LINE:
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Exercise for lab 4, Part 2");
+    // Variable Assignment
+    let firstNumber;
+    let secondNumber;
+    let total;
+
+    // Variable Assignments
+    firstNumber = prompt("Enter a number:");
+    secondNumber = prompt("Enter a second Number:");
+    
+    // Calculate total
+    total = Number(firstNumber) + Number(secondNumber);
+
+    // Display output variables
+    document.writeln("First Number: " + firstNumber);
+    document.writeln("Second Number: " + secondNumber);
+    document.writeln("Concatenation: " + firstNumber + secondNumber);
+    document.writeln("Addition Total: " + total);
 }
