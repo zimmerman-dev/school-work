8c51955393daebd4223d2eba806375be1a958a4e
John Zimmerman <zimmerman-dev@outlook.com>
2026-02-14 20:56:22 -0600

    Finished labs 3-5

--- Intro Prog JS/LearningUnits/Unit2/unit02/unit02/js/lab-04-additiveOperators.js
+++ Intro Prog JS/LearningUnits/Unit2/unit02/unit02/js/lab-04-additiveOperators.js
@@ -1,9 +1,29 @@
 function lab04() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
-    
+    // Variable Declarations
+    let addTotal;
+    let subtractTotal;
+    let total;
+    let numberOne;
+    let numberTwo;
+    let numberThree;
+    let numberFour;
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Lab 4");
+    // Assign variables with prompt function
+    numberOne = Number(prompt("Enter a value for numberOne:"));
+    numberTwo = Number(prompt("Enter a value for numberTwo:"));
+    numberThree = Number(prompt("Enter a value for numberThree:"));
+    numberFour = Number(prompt("Enter a value for numberFour:"));
+
+    // Calculate totals
+    addTotal = numberOne + numberTwo;
+    subtractTotal = numberThree - numberFour;
+    total = addTotal + subtractTotal;
+
+    // Output section, display totals
+    document.writeln("addTotal: " + addTotal);
+    document.writeln("subtractTotal: " + subtractTotal);
+    document.writeln(""); // empty line
+    document.writeln("total: " + total);
 }
--- Intro Prog JS/LearningUnits/Unit2/unit02/unit02/js/lab-05-multiplicativeOperators.js
+++ Intro Prog JS/LearningUnits/Unit2/unit02/unit02/js/lab-05-multiplicativeOperators.js
@@ -1,9 +1,42 @@
 function lab05() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
+    // Variable Declarations
+    let widgetPrice;
+    let widgetQuantity;
+    let widgetTotal;
+    let dodadPrice;
+    let dodadQuantity;
+    let dodadTotal;
+    let gadgetPrice;
+    let gadgetQuantity;
+    let gadgetTotal;
+    let totalPrice;
+    let totalQuanity;
+    let weightedMean;
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Lab 5");
-    
+    // Variable Assignment
+    widgetPrice = Number(prompt("Enter a price for the widget:"));
+    widgetQuantity = Number(prompt("Enter a quanity:"));
+    dodadPrice = Number(prompt("Enter a price for dodad:"));
+    dodadQuantity = Number(prompt("Enter a quanity:"));
+    gadgetPrice = Number(prompt("Enter a price for gadget:"));
+    gadgetQuantity = Number(prompt("Enter a quanity:"));
+
+    // Calculations for totals and average price
+    widgetTotal = widgetPrice * widgetQuantity;
+    dodadTotal = dodadPrice * dodadQuantity;
+    gadgetTotal = gadgetPrice * gadgetQuantity;
+    totalPrice = widgetTotal + dodadTotal + gadgetTotal;
+    totalQuanity = widgetQuantity + dodadQuantity + gadgetQuantity;
+    weightedMean = totalPrice / totalQuanity;
+
+    // Display your findings
+    document.writeln("Widgets: " + widgetQuantity + " @ " + widgetPrice + " for $" + widgetTotal);
+    document.writeln("Dodads: " + dodadQuantity + " @ $" + dodadPrice + " for $" + dodadTotal);
+    document.writeln("Gadget: " + gadgetQuantity + " @ $" + gadgetPrice + " for $" + gadgetTotal);
+
+    document.writeln("Total Price: $" + totalPrice);
+    document.writeln(""); // line space
+    document.writeln("Average Price (weighted mean): $" + weightedMean);
 }
