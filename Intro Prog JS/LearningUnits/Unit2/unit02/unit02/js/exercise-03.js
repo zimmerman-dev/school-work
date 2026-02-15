9124af3dadb10fd96a65ce07698f7e117cdaffab
John Zimmerman <zimmerman-dev@outlook.com>
2026-02-14 21:27:22 -0600

    finished unit 2 challege 03

--- Intro Prog JS/LearningUnits/Unit2/unit02/unit02/js/exercise-03.js
+++ Intro Prog JS/LearningUnits/Unit2/unit02/unit02/js/exercise-03.js
@@ -1,15 +1,37 @@
 function exercise03Part1() {
     "use strict";
-    // PART 1: YOUR CODE STARTS AFTER THIS LINE:
+    // Variable Declarations
+    let enteredNumber;
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Exercise for lab 3, Part 1");
+    // Prompt user for a number and display the result
+    enteredNumber = Number(prompt("Enter a number:"));
+    document.writeln("You entered the number: " + enteredNumber);
+
+    // Prompt user again
+    enteredNumber = Number(prompt("Enter a number:"));
+    document.writeln("Then you entered the number: " + enteredNumber);
 }
 
 function exercise03Part2() {
     "use strict";
-    // PART 2: YOUR CODE STARTS AFTER THIS LINE:
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Exercise for lab 3, Part 2");
+    // Variable Declarations
+    let firstName;
+    let numberOne;
+    let numberTwo;
+    let total;
+
+    // Variable Assignment
+    firstName = "Fred";
+    numberOne = 10;
+    numberTwo = 20;
+
+    // Calculations
+    total = numberOne + numberTwo;
+
+    // Display all the values collected
+    document.writeln("Name: " + firstName);
+    document.writeln("First number: " + numberOne);
+    document.writeln("Second number: " + numberTwo);
+    document.writeln("Total: " + total);
 }
