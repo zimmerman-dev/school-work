f312b06110abbc11463c75bab2a95d7d31eeb15e
John Zimmerman <zimmerman-dev@outlook.com>
2026-02-23 20:26:39 -0600

    ITPJS: project 2 part 3 complete

--- Intro Prog JS/LearningUnits/Unit2/unit02/unit02/js/project-part03.js
+++ Intro Prog JS/LearningUnits/Unit2/unit02/unit02/js/project-part03.js
@@ -1,9 +1,23 @@
 function part03() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
+    // Declare constants
+    const DAYS_IN_YEAR = 365;
+    const AVERAGE_DAYS_IN_MONTH = 30.4;
 
-    // Delete this line before starting (for testing purposes)
-    document.write("part03");
-    
+    // Declare Variables
+    let ageYears;
+    let remainingMonths;
+    let ageDays;
+
+    // Variable assignment (prompt user)
+    ageYears = Number(prompt("Enter your age in years as a whole number:"));
+    remainingMonths = Number(prompt("Enter the remaining months of your age:"));
+
+    // Calculate the age in days
+    ageDays = (ageYears * DAYS_IN_YEAR) + (AVERAGE_DAYS_IN_MONTH * remainingMonths);
+
+    // Display the starting age and the age in days
+    document.writeln("You are " + ageYears + " years and " + remainingMonths + " months old.");
+    document.writeln("That's " + ageDays + " days! Wow!");
 }
