b0b5b9136b494fc4fa317a748a286111acc3a1f0
John Zimmerman <zimmerman-dev@outlook.com>
2026-02-23 19:57:46 -0600

    ITPJS: project 2 part 2 complete

--- Intro Prog JS/LearningUnits/Unit2/unit02/unit02/js/project-part02.js
+++ Intro Prog JS/LearningUnits/Unit2/unit02/unit02/js/project-part02.js
@@ -1,9 +1,21 @@
 function part02() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
+    // Declare constants for the temperature offset and ratio
+    const OFFSET = 32;
+    const RATIO = (5 / 9);
 
-    // Delete this line before starting (for testing purposes)
-    document.write("part02");
-    
+    // Declare Variables
+    let fahrenheit;
+    let celsius;
+
+    // Prompt User for temperature in Fahrenheit
+    fahrenheit = Number(prompt("Enter temperature in fahrenheit"));
+
+    // Calculate the the temperature in Celsius
+    celsius = (fahrenheit - OFFSET) * (RATIO);
+
+    // Display the temperature in Fahrenheit and Celsius
+    document.writeln("Entered Fahrenheit Temperature: " + fahrenheit);
+    document.writeln("Calculated Celsius Temperature: " + celsius.toFixed(0));
 }
