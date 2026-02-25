de8e4701cacc09bbbef21b5ddb75c8364c0aec01
John Zimmerman <zimmerman-dev@outlook.com>
2026-02-24 23:01:59 -0600

    ITPJS: unit3 lab 2 complete

--- Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/lab-02-esLint.js
+++ Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/lab-02-esLint.js
@@ -1,25 +1,22 @@
 function lab02() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
-
     // Declare Variables
-  let aFineVariable;
-    let anotherVariable;
-    let oneMoreVariable
+    // let aFineVariable;
+    // let anotherVariable;
+    let oneMoreVariable;
 
     // Assign Variables
-    aFineVariable = "A fine value";
-    anotherVariable ="This is another fine value"
-    oneMoreVariable = prompt("Enter the number 5" );
+    // aFineVariable = "A fine value";
+    // anotherVariable = "This is another fine value";
+    oneMoreVariable = prompt("Enter the number 5");
 
-    oneMoreVariable = Number( oneMoreVariable );
+    oneMoreVariable = Number(oneMoreVariable);
 
     //Logic section
-    if (oneMoreVariable == 5){
-        document.write("Thanks, that's just the right number. I'm happy now!");
+    if (oneMoreVariable === 5) {
+        document.writeln("Thanks, that's just the right number. I'm happy now!");
+    } else {
+        document.writeln("That's not the right number");
     }
-    else{
-    document.write("That's not the right number");
-    }; 
 }
