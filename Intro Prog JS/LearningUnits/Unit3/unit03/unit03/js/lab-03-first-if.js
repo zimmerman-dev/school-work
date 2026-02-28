7ae3a6d4c850910ba38a9239629692ed98e50191
John Zimmerman <zimmerman-dev@outlook.com>
2026-02-27 21:51:29 -0600

    ITPJS: lab 3 complete

--- Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/lab-03-first-if.js
+++ Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/lab-03-first-if.js
@@ -1,9 +1,19 @@
 function lab03() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
+    // Declare Constants
+    const ERROR_MESSAGE = "Entered Number not greater than 10!";
+    const LOWER_BOUNDARY = 10;
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Lab 3");
+    // Declare Variables
+    let enteredNumber;
     
+    // Assign enteredNumber with prompt
+    enteredNumber = Number(prompt("Enter a number greater than 10:"));
+
+    // If num is less than or equal to 10, display error. Display num
+    if (enteredNumber <= LOWER_BOUNDARY) {
+        document.writeln(ERROR_MESSAGE);
+    }
+    document.writeln("Entered Number: " + enteredNumber);
 }
