d0be09d371cbde36eee3233eae056fb034118b1d
John Zimmerman <zimmerman-dev@outlook.com>
2026-03-01 03:16:01 -0600

    ITPJS: unit 3 lab 5 complete

--- Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/lab-05-simple-if-else.js
+++ Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/lab-05-simple-if-else.js
@@ -1,14 +1,34 @@
 function lab05() {
     "use strict";
 
-    // Declarations
+    // Constant Declaration
+    const ERROR_MESSAGE = "Input Error... Try again!";
+    
+    // Variable Declarations
     let firstString;
     let secondString;
     let outputMessage;
 
     // Variable Assignment
     firstString = prompt("Enter a string:");
-    secondString = prompt("Enter another:");
+    secondString = prompt("Enter a string that will sort after the first:");
 
-    
+    // Validation (check for empty strings)
+    if (firstString === null || firstString.length <= 0 
+        || secondString === null || secondString.length <= 0) {
+        alert(ERROR_MESSAGE);
+        return;
+    }
+
+    // Calculation
+    if (firstString < secondString) {
+        outputMessage = "Very good, \"" + secondString 
+        + "\" comes after \"" + firstString + "\".";
+    } else {
+        outputMessage = "\"" + secondString 
+        + "\" is the same or does not sort after \"" + firstString + "\".";
+    }
+
+    // Display
+    document.writeln(outputMessage);
 }
