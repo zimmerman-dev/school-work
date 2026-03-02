34d3410ea467c6d7e7b2d7215aac48a6ff452f74
John Zimmerman <zimmerman-dev@outlook.com>
2026-03-02 00:32:59 -0600

    ITPJS: unit 3 lab 6 complete

--- Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/lab-06-multiple-booleans.js
+++ Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/lab-06-multiple-booleans.js
@@ -1,9 +1,35 @@
 function lab06() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
+    // Constant Declaration
+    const MIN_VALUE = 1;
+    const MAX_VALUE = 100;
+    const ERROR_MESSAGE = "Input Error... Try again!";
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Lab 6");
-    
+    // Variable Declarations
+    let userNumber;
+    let outputMessage;
+
+    // Variable Assignment
+    userNumber = prompt("Enter a number between 1 and 100 exclusive:");
+
+    // Validation (check for cancel, empty string, and isNumeric)
+    if (userNumber === null || userNumber.length <= 0 || !isNumeric(userNumber)) {
+        alert(ERROR_MESSAGE);
+        return;
+    }
+
+    userNumber = Number(userNumber);
+
+    // Calculations (logic)
+    if (userNumber > MIN_VALUE && userNumber < MAX_VALUE) {
+        outputMessage = "The number " + userNumber 
+        + " is within the correct range.";
+    } else {
+        outputMessage = "The number " + userNumber 
+        + " is not within the correct range, try again.";
+    }
+
+    // Display result
+    document.writeln(outputMessage);
 }
