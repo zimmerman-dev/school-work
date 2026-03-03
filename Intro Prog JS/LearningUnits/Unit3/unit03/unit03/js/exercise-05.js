9669dbc3129fe803aa139ec7f88d28ea957e6c39
John Zimmerman <zimmerman-dev@outlook.com>
2026-03-02 21:59:05 -0600

    ITPJS: unit 3 challenge 5 modified

--- Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/exercise-05.js
+++ Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/exercise-05.js
@@ -1,9 +1,41 @@
 function exercise05Part1() {
     "use strict";
-    // PART 1: YOUR CODE STARTS AFTER THIS LINE:
+    // Const Declaration
+    const ERROR_MESSAGE = "Input Error. Try again.";
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Exercise for lab 5, Part 1");
+    // Variable Declaration
+    let userNumOne;
+    let userNumTwo;
+    let outputMessage;
+
+    // Prompt User
+    userNumOne = prompt("Enter a number:");
+
+    // Validate userNumOne
+    if (userNumOne === null || userNumOne.length <= 0 || !isNumeric(userNumOne)) {
+        alert(ERROR_MESSAGE);
+    }
+
+    // Prompt User
+    userNumTwo = prompt("Enter another number:");
+
+    // Validate userNumTwo
+    if (userNumTwo === null || userNumTwo.length <= 0 || !isNumeric(userNumTwo)) {
+        alert(ERROR_MESSAGE);
+    }
+
+    userNumOne = Number(userNumOne);
+    userNumTwo = Number(userNumTwo);
+
+    // Logic
+    if (userNumOne >= userNumTwo) {
+        outputMessage = userNumOne + " is greater than or equal to " + userNumTwo;
+    } else {
+        outputMessage = userNumOne + " is less than " + userNumTwo;
+    }
+
+    // Display
+    document.writeln(outputMessage);
 }
 
 function exercise05Part2() {
