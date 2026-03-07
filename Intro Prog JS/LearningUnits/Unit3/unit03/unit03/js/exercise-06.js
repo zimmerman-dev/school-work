35efb0415fe1a35e22e4cd00072d06aab25a6bd8
John Zimmerman <zimmerman-dev@outlook.com>
2026-03-07 05:24:42 -0600

    ITPJS: unit 3 challenge exercise 6 p1 complete

--- Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/exercise-06.js
+++ Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/exercise-06.js
@@ -1,9 +1,35 @@
 function exercise06Part1() {
     "use strict";
-    // PART 1: YOUR CODE STARTS AFTER THIS LINE:
+    // Constant Declaration
+    const COUNTY_CHECK_DANE = "dane";
+    const COUNTY_CHECK_SAUK = "sauk";
+    const ERROR_MESSAGE = "Input Error. Try again.";
+    
+    // Variable Declaration
+    let queriedCounty;
+    let outputMessage;
+    
+    // Prompt user
+    queriedCounty = prompt("Enter what county you are from:");
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Exercise for lab 6, Part 1");
+    // Validate
+    if (queriedCounty === null || queriedCounty.length <= 0 || isNumeric(queriedCounty)) {
+        alert(ERROR_MESSAGE);
+        return;
+    }
+
+    queriedCounty = queriedCounty.toLowerCase();
+
+    // Logic for county check
+    if (queriedCounty === COUNTY_CHECK_DANE || queriedCounty === COUNTY_CHECK_SAUK) {
+        outputMessage = "Hello, please donate to the park cleanup fund.";
+    } else {
+        outputMessage = "Hello, welcome to the county.";
+    }
+
+    // Display output message
+    document.writeln(outputMessage);
+
 }
 
 function exercise06Part2() {
