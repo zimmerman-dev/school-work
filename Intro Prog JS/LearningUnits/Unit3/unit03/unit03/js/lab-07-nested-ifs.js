969d1be8f06e9022c464d9d16f813c4f0ddca18c
John Zimmerman <zimmerman-dev@outlook.com>
2026-03-08 22:02:00 -0500

    ITPJS: unit 3 lab 7 complete

--- Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/lab-07-nested-ifs.js
+++ Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/lab-07-nested-ifs.js
@@ -1,19 +1,128 @@
 function lab07Part01() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
+    // Constant Declaration
+    const ERROR_MESSAGE = "Input error, try again";
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Lab 7 Part 1");
-    
+    // Variable Declaration
+    let enteredName;
+    let enteredCode;
+    let outputMessage;
+    // Prompt user
+    enteredName = prompt("Enter your name:");
+    enteredCode = prompt("Enter a code A, B, C, or D:");
+
+    // Validate
+    if (enteredName === null 
+        || enteredName.length <= 0 
+        || isNumeric(enteredName) 
+        || enteredCode === null 
+        || enteredCode.length <= 0 
+        || isNumeric(enteredCode)) {
+        alert(ERROR_MESSAGE);
+        return;
+    }
+
+    enteredCode = enteredCode.toUpperCase();
+
+    // Logic
+    if (enteredCode === "A") {
+        outputMessage = "Hello, " + enteredName + ", how are you?";
+    } else if (enteredCode === "B") {
+        outputMessage = "Top of the morning to you, " + enteredName + "!";
+    } else if (enteredCode === "C") {
+        outputMessage = "Hi, " + enteredName + ".";
+    } else if (enteredCode === "D") {
+        outputMessage = enteredName + " is here! Welcome!";
+    } else {
+        outputMessage = ERROR_MESSAGE;
+    }
+
+    // Display
+    document.writeln(outputMessage);
 }
 
 function lab07Part02() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
+    // Constant Declaration
+    const VANILLA = "V";
+    const CHOCOLATE = "C";
+    const STRAWBERRY = "S";
+    const ERROR_MESSAGE = "We don't have that ice cream";
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Lab 7 Part 2");
+    //Variable Declaration
+    let iceCreamChoice;
+    let toppingChoice;
+    let iceCream;
+    let topping;
+    let outputMessage;
+
+    // Prompt for ice cream
+    iceCreamChoice = prompt("Choose your ice cream:\n" 
+        + "V = Vanilla\n" 
+        + "C = Chocolate\n" 
+        + "S = Strawberry");
     
+    iceCreamChoice = iceCreamChoice.toUpperCase();
+
+    // Logic calculation
+    if (iceCreamChoice === VANILLA) {
+        iceCream = "vanilla";
+
+        toppingChoice = prompt("Choose a topping:\n" 
+            + "M = M&Ms\n" 
+            + "RP = Reese's Pieces");
+        
+        toppingChoice = toppingChoice.toUpperCase();
+
+        if (toppingChoice === "M") {
+            topping = "M&Ms";
+        } else if (toppingChoice === "RP") {
+            topping = "Reese's Pieces";
+        } else {
+            topping = "no toppings";
+        }
+    } else if (iceCreamChoice === CHOCOLATE) {
+        iceCream = "chocolate";
+
+        toppingChoice = prompt("Choose a topping:\n" 
+            + "B = Brownies\n" 
+            + "W = Walnuts");
+        
+        toppingChoice = toppingChoice.toUpperCase();
+
+        if (toppingChoice === "B") {
+            topping = "brownies";
+        } else if (toppingChoice === "W") {
+            topping = "walnuts";
+        } else {
+            topping = "no toppings";
+        }
+    } else if (iceCreamChoice === STRAWBERRY) {
+        iceCream = "strawberry";
+
+        toppingChoice = prompt("Choose a topping:\n" 
+            + "P = Pecans\n" 
+            + "A = Almonds");
+        
+        toppingChoice = toppingChoice.toUpperCase();
+
+        if (toppingChoice === "P") {
+            topping = "pecans";
+        } else if (toppingChoice === "A") {
+            topping = "almonds";
+        } else {
+            topping = "no toppings";
+        }
+    } else {
+        alert(ERROR_MESSAGE);
+    }
+
+    // build final message
+    outputMessage = "You are having " 
+    + iceCream + " ice cream with " 
+    + topping + ". Enjoy!";
+
+    document.writeln(outputMessage);
 }
