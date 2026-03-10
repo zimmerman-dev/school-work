54971675a7a007a7a0ce5521b1607d2bacb75c0d
John Zimmerman <zimmerman-dev@outlook.com>
2026-03-09 19:13:52 -0500

    Web Dev: swap project challenge complete

--- Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/project-part03.js
+++ Intro Prog JS/LearningUnits/Unit3/unit03/unit03/js/project-part03.js
@@ -1,9 +1,25 @@
 function part03() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
-
-    // Delete this line before starting (for testing purposes)
-    document.write("part03");
+    // YOUR CODE STARTS AFTER THIS LINE
     
+    let v1;
+    let v2;
+    let result;
+
+    v1 = 5;
+    v2 = 8;
+    result = 0;
+
+    if (v1 === 5) {
+        if (v2 === 7) {
+            result = 1;
+        }
+    } else {
+        result = 3;
+    }
+
+    document.writeln(result);
+
+
 }
