c4491dfa3c6d7a17448d766b871a02a51c73c7c6
John Zimmerman <zimmerman-dev@outlook.com>
2026-04-15 00:21:20 -0500

    ITPJS: Unit 4 lab 9 finished

--- Intro Prog JS/learningUnits/unit-4/unit04/js/lab-09-calling-functions.js
+++ Intro Prog JS/learningUnits/unit-4/unit04/js/lab-09-calling-functions.js
@@ -1,19 +1,51 @@
+// Funky town
+function displayMessage() {
+    document.writeln("Hello, Function!");
+}
+
 function lab09Part01() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
+    // Call displayMessage function
+    displayMessage();
+    displayMessage();
+    displayMessage();
+    displayMessage();  
+}
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Lab 9 Part 1");
-    
+// Function Definition
+function displayName() {
+
+    // Variable Declaration
+    let firstName;
+    let middleInitial;
+    let lastName;
+
+    //Variable Assignment
+    firstName = prompt("Enter your first name:");
+    middleInitial = prompt("Enter your middle initial:");
+    lastName = prompt("Enter your last name:");
+
+    // Display Name
+    document.write(firstName + " " + middleInitial + ". " + lastName);
 }
 
 function lab09Part02() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
+    // Display message start
+    document.write("Hello, ");
+    
+    // Function call for name
+    displayName();
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Lab 9 Part 2");
+    // Display message end
+    document.writeln(". How are you?");
+
+    // function call for name
+    displayName();
+
+    // Display message end
+    document.write(", has left the building.");
     
 }
