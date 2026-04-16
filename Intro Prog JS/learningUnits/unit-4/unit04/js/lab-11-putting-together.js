a1eec1688d87ddafa59df4ff1a6a66b0be7272a2
John Zimmerman <zimmerman-dev@outlook.com>
2026-04-15 21:29:57 -0500

    ITPJS: Unit 4 lab 11 finished

--- Intro Prog JS/learningUnits/unit-4/unit04/js/lab-11-putting-together.js
+++ Intro Prog JS/learningUnits/unit-4/unit04/js/lab-11-putting-together.js
@@ -1,3 +1,37 @@
+// Global Constants
+const NUMBER_TIMES_TO_LOOP = 5;
+
+// Global Variables
+let loopCounter;
+let sum;
+let userEnteredNumber;
+
+// Initialize loop
+function initializeForLooping() {
+    loopCounter = 0;
+    sum = 0;
+}
+
+// Prompt user for number
+function obtainNumberFromUser() {
+    userEnteredNumber = prompt("Enter a number:");
+    userEnteredNumber = Number(userEnteredNumber);
+}
+
+// Print entered number to screen
+function displayNumberOnWebPage() {
+    document.writeln("Entered number was: " + userEnteredNumber);
+}
+
+// Adding the user entered number to sum
+function addPositiveNumberToSum() {
+    sum += userEnteredNumber;
+}
+
+// Print total
+function displayPositiveNumbersSum() {
+    document.writeln("Total: " + sum);
+}
 
 /*
     The purpose of this function is to run lab 11 part 1
@@ -7,18 +41,17 @@ function lab11Part01() {
 
     initializeForLooping();
 
-    while (loopCounter < NUMBER_TIMES_TO_LOOP)  {
+    while (loopCounter < NUMBER_TIMES_TO_LOOP) {
         obtainNumberFromUser();
         displayNumberOnWebPage();
 
-        if (userEnteredNumber > 0)  {
+        if (userEnteredNumber > 0) {
             addPositiveNumberToSum();
         }
         loopCounter++;
     }
 
-    displayPositiveNumbersSum();
-    
+    displayPositiveNumbersSum();   
 }
 
 /*********************************************************
@@ -26,6 +59,37 @@ function lab11Part01() {
         Separation comment between Part 1 and Part 2 
 **********************************************************
 *********************************************************/
+
+// Global Variables
+let records;
+let hourlyWage;
+let counter;
+let minimumHourlyWage;
+let maximumHourlyWage;
+let averageHourlyWage;
+let grandSumHourlyWages;
+
+// Function to assign variables
+function assignInitialValuesToVariables() {
+    counter = 0;
+    minimumHourlyWage = Infinity;
+    maximumHourlyWage = -Infinity;
+    averageHourlyWage = 0;
+    grandSumHourlyWages = 0;
+}
+
+// Function to calculate average wages
+function calculateAverageHourlyWage() {
+    averageHourlyWage = grandSumHourlyWages / counter;
+    averageHourlyWage = averageHourlyWage.toFixed(2);
+}
+
+// Function to display the results
+function displayMinimumMaximumAverageHourlyWage() {
+    document.writeln("Average Hourly Wage: " + averageHourlyWage);
+    document.writeln("Maximum Hourly Wage: " + maximumHourlyWage);
+    document.writeln("Minimum Hourly Wage: " + minimumHourlyWage);
+}
 
 /*
     The purpose of this function is to run lab 11 part 2
@@ -37,7 +101,7 @@ function lab11Part02() {
 
     records = openEmployeePayrollRecords();
 
-    while ( records.readNextRecord() ) {
+    while (records.readNextRecord()) {
         counter++;
         hourlyWage = records.getEmployeeHourlyWage();
         grandSumHourlyWages += hourlyWage;
@@ -46,7 +110,9 @@ function lab11Part02() {
             minimumHourlyWage = hourlyWage;
         }
 
-        // here write the IF to handle max hourly wage
+        if (hourlyWage > maximumHourlyWage) {
+            maximumHourlyWage = hourlyWage;
+        }
 
     }
 
