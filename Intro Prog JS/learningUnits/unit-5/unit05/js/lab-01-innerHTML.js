361a8cc775d9536fabd2d3fb8c0bc6346d80ebaf
John Zimmerman <zimmerman-dev@outlook.com>
2026-04-20 10:39:13 -0500

    ITPJS: catchup

--- Intro Prog JS/learningUnits/unit-5/unit05/js/lab-01-innerHTML.js
+++ Intro Prog JS/learningUnits/unit-5/unit05/js/lab-01-innerHTML.js
@@ -1,9 +1,20 @@
 function lab01() {
     "use strict";
 
-    // YOUR CODE STARTS AFTER THIS LINE:
+    // Variable Declaration
+    let clientTable;
+    let clientRow;
 
-    // Delete this line before starting (for testing purposes)
-    document.write("Lab 1");
+    // Assign Variables
+    clientTable = document.getElementById("clientListBody");
+    clientRow = "";
+
+    // Client Input
+    clientRow += "<tr><td>Alice</td><td>Crane</td><td>23</td></tr>";
+    clientRow += "<tr><td>Steve</td><td>Willis</td><td>30</td></tr>";
+    clientRow += "<tr><td>James</td><td>Smith</td><td>10</td></tr>";
+    clientRow += "<tr><td>Melissa</td><td>Frank</td><td>45</td></tr>";
+
+    clientTable.innerHTML = clientRow;
 
 }
